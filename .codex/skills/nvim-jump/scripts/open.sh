40f95d81597db9cbaed7063df67730fd8db7faf4
Mike Kozlowski <mlk@klio.org>
2025-12-29 21:39:19 -0500

    handle errors better

--- .codex/skills/nvim-jump/scripts/open.sh
+++ .codex/skills/nvim-jump/scripts/open.sh
@@ -7,4 +7,7 @@ col="${3:-1}"
 server="${NVIM_SERVER:-/tmp/nvimsocket}"
 
 # Requires: nvr (neovim-remote)
-nvr --servername "$server" +"call cursor(${line},${col})" "$file"
+if ! nvr --servername "$server" +"call cursor(${line},${col})" "$file"; then
+  # Fall back to a new tab to avoid "no write since last change" errors.
+  nvr --servername "$server" --remote-tab +"call cursor(${line},${col})" "$file"
+fi
